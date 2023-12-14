95bb6d0b224c40ac0a81ec493e0a74816c123c94
Nabilety <37532159+Nabilety@users.noreply.github.com>
2023-12-14 08:55:50 +0100

    Search for books given a query

--- search.py
+++ search.py
@@ -2,6 +2,12 @@ import gzip
 import json
 import pandas
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+import re
+
+#pd.set_option('display.max_columns', None)
 
 # Explore data
 with gzip.open("goodreads_books.json.gz", 'r') as f:
@@ -38,8 +44,39 @@ with gzip.open("goodreads_books.json.gz", "r") as f:
         if ratings > 15:
             books_titles.append(fields)
 
+# Processing book metadata with pandas
 titles = pd.DataFrame.from_dict(books_titles) # Turn each dictionary in books_title into a row in the dataframe
 titles["ratings"] = pd.to_numeric(titles["ratings"])
-titles["mod_title"] = titles["title"].str.replace("[^a-zA-Z0-9]", "", regex=True)
-print(titles)
+titles["mod_title"] = titles["title"].str.replace("[^a-zA-Z0-9 ]", "", regex=True)
+titles["mod_title"] = titles["mod_title"].str.lower()
+titles["mod_title"] = titles["mod_title"].str.replace("\s+", " ", regex=True)
+titles = titles[titles["mod_title"].str.len() > 0] # Only take rows where len of mod_title greater than 0
+titles.to_json("books_titles.json")
 
+#with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    #print(titles.head(3))
+
+# minimize the impact of common words using inverse document frequency matrix
+# (equation is log(number of different titles / divided by the number of titles that particular word appear in.
+# Lastly multiplying term frequencies with inverse document frequencies)
+# This technique is called tf idf.
+
+# Vectorizer takes list of strings and turns it into tf idf matrix
+vectorizer = TfidfVectorizer()
+
+tfidf = vectorizer.fit_transform(titles["mod_title"])
+
+# Build code to turn search query into vector and match against our matrix
+def search(query, vectorizer):
+    #query = "fire upon the deep" # query
+    processed = re.sub("[^a-zA-Z0-9 ]", "", query.lower()) # 2. process query
+    query_vec = vectorizer.transform([processed]) # turn query into vector
+    similarity = cosine_similarity(query_vec, tfidf).flatten() # search the tfidf matrix for our search query and tell us how much each row in the matrix is similar
+    indices = np.argpartition(similarity, -10)[-10:] # from the above matrix, find the indices of the 10 largest similarities values
+    results = titles.iloc[indices]
+    results = results.sort_values("ratings", ascending=False) # incase of duplicate titles, take the rows with highest ratings
+    return  results.head(5)
+
+print(search("East of Eden", vectorizer))
+
+#Create a list of liked books
